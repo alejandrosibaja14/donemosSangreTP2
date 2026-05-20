1df6b3fdf55d82b39db37f0dd58e8923ccc9ea5a
alejandrosibaja14 <sibaja.alejandro@icloud.com>
2026-05-19 19:05:17 -0600

    Creación de funición inicial Menú. Avance #1

---
+++ donemosSangre.py
@@ -0,0 +1,37 @@
+#Creado por: Alejandro Sibaja Badilla y Marco Herrera Gómez
+#Fecha de creación: 16/05/2026
+#Ultima actualización: xx/05/2026
+#Versión de python: 3.14
+#Definición de funciones
+def mostrarMenu():
+    print("\n===== DONEMOS SANGRE =====")
+    print("1. Insertar donante")
+    print("2. Eliminar donante")
+    print("3. Insertar lugar de donación")
+    print("4. Reportes")
+    print("5. Salir")
+
+def main():
+    tiposDeSangre=("A+","A-","B+","B-","AB+","AB-","O+","O-")
+    lugaresDeDonacion={"San José":[], "Alajuela":[], "Cartago":[], "Heredia":[], "Guanacaste":[],
+    "Puntarenas":[], "Limón":[]}
+    donadores=[]
+    while True:
+        mostrarMenu()
+        opcion=input("Seleccione la opción que desee ejecutar: ")
+        if opcion=="1":
+            print("Insertar donante pendiente.")
+        elif opcion=="2":
+            print("Eliminar donante pendiente.")
+        elif opcion=="3":
+            print("Insertar lugar de donación pendiente.")
+        elif opcion=="4":
+            print("Reportes pendientes.")
+        elif opcion=="5":
+            print("Donar sangre, es donar vida")
+            break
+        else:
+            print("Opción inválida.")
+
+#Inicio del programa principal
+main()
